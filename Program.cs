b01738804cae4e06a8bd2f76d5925b4555f11130
Tricia Amheiser <tricia@taarts.net>
2022-09-12 21:35:14 -0400

    Acquires simple string from the API

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace OneListClientReDo
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        // static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var responseBodyAsString = await client.GetStringAsync("https://one-list-api.herokuapp.com/items?access_token=cohort24");
+
+            Console.WriteLine(responseBodyAsString);
+        }
     }
-  }
 }
