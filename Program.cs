e7236e369c7b469048288ad65f3b798c97de0e11
Tricia <tricia@taarts.net>
2022-09-28 12:11:33 -0400

    Make a client | get a string

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Net.Http;
-
+using System.Threading.Tasks;
 
 namespace OneListClientReDo
 {
     class Program
-    {
-        static void Main(string [] args)
+    {   
+        // previously "static void..."
+        static async Task Main(string [] args)
         {
-            var client = new HttpClient();
+            var client = new HttpClient(); /* make a client */
+
+            var responseBodyAsString = await client.GetStringAsync("https://one-list-api.herokuapp.com/items?access_token=cohort24"); /* get a string */
+
+            // this code will happen at the SAME TIME our network request is going.
+            // add await and the "static" environment changes 
+
+            Console.WriteLine(responseBodyAsString);
+            
         }
+    }
 }
