c718e37df693d326e6e14ae0a0385807860db100
Tricia <tricia@taarts.net>
2022-09-28 18:45:38 -0400

    Custom DateTime | Customer Completed Status

--- Item.cs
+++ Item.cs
@@ -1,12 +1,33 @@
+using System;
+
 namespace OneListClientReDo
 {
     public class Item
     {
         // not in Pascal case - ruby convention not C#
+    // If you trust the api then let the information be converted by the method
         public int id { get; set; }
         public string text { get; set; }
         public bool complete { get; set; }
-        public string created_at { get; set; }
-        public string updated_at { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+
+        public string CompletedStatus 
+        {
+            // custom "get" for a property about to be a turnery.
+            get {
+                // TURNERY statement
+                // return Boolean expression ? value when true : value when false; <-- inline if/else
+                return complete ? "Completed" : "Not Completed";
+            //     if (complete == true)
+            //     {
+            //         return "Completed";
+            //     }
+            //     else
+            //     {
+            //         return "Not completed";
+            //     }
+            }
+        }
     }
 }
