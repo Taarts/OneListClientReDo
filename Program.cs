ba4a6957f5cc57aa6ec7675b91570acf925745dd
Tricia <tricia@taarts.net>
2022-09-29 10:25:35 -0400

    Add one todo... not working despite c/v

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -10,6 +11,39 @@ namespace OneListClientReDo
 {
     class Program
     {   
+        static async Task AddOneItem(string token, Item newItem)
+    {
+    var client = new HttpClient();
+
+    // Generate a URL specifically referencing the endpoint for adding a todo item
+    var url = $"https://one-list-api.herokuapp.com/items?access_token={token}";
+
+    // Take the `newItem` and serialize it into JSON
+    var jsonBody = JsonSerializer.Serialize(newItem);
+
+    // We turn this into a StringContent object and indicate we are using JSON
+    // by ensuring there is a media type header of `application/json`
+    var jsonBodyAsContent = new StringContent(jsonBody);
+    jsonBodyAsContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+    // Send the POST request to the URL and supply the JSON body
+    var response = await client.PostAsync(url, jsonBodyAsContent);
+
+    // Get the response as a stream.
+    var responseJson = await response.Content.ReadAsStreamAsync();
+
+    // Supply that *stream of data* to a Deserialize that will interpret it as a *SINGLE* `Item`
+    var item = await JsonSerializer.DeserializeAsync<Item>(responseJson);
+
+    // Make a table to output our new item.
+    var table = new ConsoleTable("ID", "Description", "Created At", "Updated At", "Completed");
+    
+    // Add one row to our table
+    table.AddRow(item.Id, item.Text, item.CreatedAt, item.UpdatedAt, item.CompletedStatus);
+
+    // Write the table
+    table.Write(Format.Minimal);
+}
         static async Task GetOneItem(string token, int id)
         {
             try{
@@ -81,13 +115,29 @@ namespace OneListClientReDo
             while (keepGoing)
             {
                 Console.Clear();
-                Console.Write("Get (A)ll todo, (O)ne todo, or (Q)uit: ");
+                Console.Write("Get (A)ll todo, (C)reate an item, (O)ne todo, or (Q)uit: ");
                 var choice = Console.ReadLine().ToUpper();
                 switch (choice)
                 {
                     case "Q":
                         keepGoing = false;
                         break;
+
+                    case "C":
+                    Console.Write("Enter the description of your new todo: ");
+                    var text = Console.ReadLine();
+
+                    var newItem = new Item
+                    {
+                    Text = text
+                    };
+
+                    await AddOneItem(token, newItem);
+
+                    Console.WriteLine("Press ENTER to continue");
+                    Console.ReadLine();
+                    break;
+
                     case "O":
                         Console.Write("Enter the ID ");
                         var id = int.Parse(Console.ReadLine());
